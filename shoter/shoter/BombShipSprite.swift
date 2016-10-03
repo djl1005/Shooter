afcb6b7251bcfae69feae4b9a7997ef72185bbe2
Sung Choi <swc1098@rit.edu>
2016-10-03 15:04:45 -0400

    BombShip now fires.

--- shoter/shoter/BombShipSprite.swift
+++ shoter/shoter/BombShipSprite.swift
@@ -15,6 +15,7 @@ class BombShipSprite: SKSpriteNode {
     var canLaunch = true
     var isFiring = false
     var health = 10
+    let bullet = SKSpriteNode(imageNamed:"bullet.png") //TODO: MAKE IT DIFFERENT FROM PLAYER'S
     
     init(){
         super.init(texture: SKTexture(imageNamed:"BombShip.png"),  color: GameData.player.playerColor, size: GameData.player.playerSize);
@@ -48,12 +49,12 @@ class BombShipSprite: SKSpriteNode {
         
         
         // move to the end of screen in 3 seconds, maintaining y position
-        let actionApproach = SKAction.move(to: CGPoint(x: self.position.x + 1600, y: self.position.y), duration: TimeInterval(bombApproachSpeed))
+        let actionApproach = SKAction.move(to: CGPoint(x: self.position.x + 1500, y: self.position.y), duration: TimeInterval(bombApproachSpeed))
         
         // rotate by 90 degrees
         let actionRotate = SKAction.rotate(byAngle: CGFloat(M_PI_2), duration: 3.0)
         
-        let actionFlyUp = SKAction.move(to: CGPoint(x: self.position.x + 1600, y: self.position.y + 900), duration: TimeInterval(raidSpeed))
+        let actionFlyUp = SKAction.move(to: CGPoint(x: self.position.x + 1500, y: self.position.y + 900), duration: TimeInterval(raidSpeed))
         
         let actionFlyBack = SKAction.move(to: CGPoint(x: self.position.x, y: self.position.y + 900), duration: TimeInterval(bombApproachSpeed))
         
@@ -62,11 +63,49 @@ class BombShipSprite: SKSpriteNode {
         run(SKAction.sequence([
             actionApproach,
             actionRotate,
+            fireAction(),
             actionFlyUp,
+            fireStop(),
             actionRotate,
             actionFlyBack,
             actionDone
             ])
         )
+    }
+    //func fire(){
+        //bullet = SKSpriteNode(imageNamed:"bullet.png") //TODO: MAKE IT DIFFERENT FROM PLAYER'S
+    //}
+    
+    func fireAction() -> SKAction{
+        return SKAction.run{
+            
+            self.bullet.position = CGPoint(x:self.position.x,y:self.position.y)
+            self.bullet.zRotation = CGFloat(-M_PI * 0.5)
+            self.bullet.size = CGSize(width: self.bullet.size.width/2, height: self.bullet.size.height/2)
+            let scene = self.parent as! SKScene
+            
+            scene.addChild(self.bullet)
+            
+            let wait = SKAction.wait(forDuration: 1.0)
+            
+            // how long until bullet reaches destination?
+            let bulletLifeTime = CGFloat(2.0)
+            
+            
+            // move to the end of screen in 3 seconds, maintaining y position
+            let fire = SKAction.move(to: CGPoint(x: 2000, y: self.position.y), duration: TimeInterval(bulletLifeTime))
+            
+            let done = SKAction.removeFromParent()
+            
+            let sequence = SKAction.repeatForever(SKAction.sequence([fire, wait, done]))
+            //print ("FIRING")
+            self.bullet.run(sequence, withKey: "Firing")
+        }
+    }
+    
+    func fireStop() -> SKAction{
+        return SKAction.run{
+            self.bullet.removeAction(forKey: "Firing")
+        }
     }
 }
